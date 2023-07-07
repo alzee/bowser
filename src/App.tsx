4a6ddf3c94fe4cdb3a692c2e899681087c4099bf
Al Zee <z@alz.ee>
2023-07-07 17:48:43 +0800

    exportJuanNei almost done

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,8 @@ function App() {
   const [msg, setMsg] = useState("")
   const [dir, setDir] = useState("")
   const [file, setFile] = useState("")
+  const [prefix, setPrefix] = useState("")
+  const sheetName = "Sheet1"
 
   listen<string>('tauri://file-drop', (event) => {
     setDir(event.payload[0])
@@ -32,8 +34,6 @@ function App() {
 
   async function getFile() {
     const file = await open({directory: false})
-    // const contents = await readBinaryFile(file)
-    // console.log(contents)
     if (file !== null) {
       console.log(file)
       setFile(file)
@@ -44,17 +44,135 @@ function App() {
     return false
   }
 
-  function processData(file) {
-    if (isXlsx(file)) {
-      //
-    } else {
-      setMsg(`[${file}] 不是有效的xls/xlsx文件`)
+  async function importXlsx(file) {
+    let data = {}
+    return data
+  }
+
+  async function exportAnJuan(aoa) {
+    const ws_data = [
+      ...[[ "案卷级档案", "姓名", "性别", "身份证号", "政治面貌", "密集架号", "总件数", '总页数' ]],
+      ...aoa
+    ]
+    console.log(ws_data);
+    const wb = utils.book_new()
+    utils.book_append_sheet(wb, utils.aoa_to_sheet(ws_data), sheetName)
+    const data = write(wb, { type: "buffer", bookType: "xlsx" })
+    await writeBinaryFile(dir + '/' + outputDir + '/人事案卷.xlsx', data)
+  }
+
+  async function extractMoreData(name, sheet) {
+    for (const key in sheet) {
+      if (sheet[key].v === name) {
+        const row = key.replace(/[A-Z]*/, '')
+
+        const gender = sheet['C' + row].v
+        const id = sheet['D' + row].v
+        const org = sheet['E' + row].v
+        const birth = sheet['F' + row].v
+        const ethnic = sheet['G' + row].v
+        const homeland = sheet['H' + row].v
+        const status = sheet['K' + row].v
+        return {
+          name,
+          gender,
+          id,
+          org,
+          birth,
+          ethnic,
+          homeland,
+          status
+        }
+      }
     }
+  }
+
+  async function extractData(sheet) {
+    const name = sheet.A2.v.split('：')[2].replace(/ /g, '')
+    const sn = sheet.A2.v.split('：')[1].replace(/[^\x00-\x7F]/g, "").replace(/ /g, '')
+
+    const ref = sheet['!ref']
+    let lastRow
+    if (ref !== undefined) {
+      lastRow = Number(ref.replace(/[A-Z]/g,'').split(':')[1])
+    } else {
+      lastRow = 0
+    }
+
+    let docs = []
+    let startRow = 6
+    let sum = 0
+    let count = 0
+
+    for (let i = startRow; i < lastRow; i++) {
+      const cell = sheet['G' + i]
+      if (cell !== undefined) {
+        const pages = Number(cell.v)
+        sum += pages
+        let date
+        if (sheet['C' + i] !== undefined) {
+          date = sheet['C' + i].v.toString()
+          date += sheet['D' + i].v.toString()
+          date += sheet['E' + i].v.toString()
+        }
+        const doc = {
+          sn,
+          cateid: sheet['A' + i].v,
+          title: sheet['B' + i].v,
+          date,
+          pages 
+        }
+        docs.push(doc)
+        count += 1
+      }
+    }
+
+    return {
+      name,
+      sn,
+      count,
+      sum,
+      docs
+    }
+  }
+
+  async function exportJuanNei(individual) {
+    let ws_data = [
+      [ "序号", "案卷号", "案卷级档号", "档号", "类号", "类别代号", "类别件号", '材料名称', '形成时间', '页数', '' ]
+    ]
+    let aoa = []
+    aoa[0] = individual.name
+    ws_data.push(aoa)
+
+    for (let i = 1; i <= individual.docs.length; i++) {
+      const doc = individual.docs[i-1]
+      aoa = []
+      aoa[0] = i
+      aoa[7] = doc.title
+      aoa[8] = doc.date
+      aoa[9] = doc.pages
+      ws_data.push(aoa)
+    }
+
+    const wb = utils.book_new()
+    utils.book_append_sheet(wb, utils.aoa_to_sheet(ws_data), sheetName)
+    const contents = write(wb, { type: "buffer", bookType: "xlsx" })
+    await writeBinaryFile(dir + '/' + outputDir + '/' + individual.name + '-人事卷内目录.xlsx', contents)
+  }
+
+  async function getSheet(path) {
+    const contents = await readBinaryFile(path)
+    const workbook = read(contents)
+    const sheeName = workbook.SheetNames[0]
+    return workbook.Sheets[sheeName]
   }
 
   async function main() {
     if (file === null || file === '') {
     } else {
+      const basicInfoSheet = await getSheet(file)
+      // const basicInfo = await extractMoreData('李志浩', basicInfoSheet)
+      // console.log(basicInfo)
     }
 
     if (dir === null || dir === '') {
@@ -67,112 +185,27 @@ function App() {
         const newDir = dir + '/' + outputDir
         await createDir(newDir, { recursive: true })
 
-        processData(file)
-
-        const sheetName = "Sheet1"
-
-        let ws_data0 = [
-          [ "案卷级档案", "姓名", "性别", "身份证号", "政治面貌", "密集架号", "总件数", '总页数' ],
-        ]
-
+        let aoa = []
 
         for (const entry of entries) {
-          // if entry is dir
-
-
-          console.log(entry)
-          let xlsxFile = entry
-
           if (entry.children === undefined) {
             // if entry is file
-            try {
-              // TODO check if is xlsx to avoid read big file
-              const contents = await readBinaryFile(dir + '/' + xlsxFile.name)
-
-              const workbook = read(contents)
-              const sheeName = workbook.SheetNames[0]
-              const sheet = workbook.Sheets[sheeName]
-              const name = sheet.A2.v.replace('姓名：', '')
-              
-              const ref = sheet['!ref']
-              let lastRow
-              if (ref !== undefined) {
-                lastRow = Number(ref.replace(/[A-Z]/g,'').split(':')[1])
-              } else {
-                lastRow = 0
-              }
-              
-              let docs = []
-              let startRow = 6
-              let sum = 0
-              let count = 0
-
-              for (let i = startRow; i < lastRow; i++) {
-                const cell = sheet['F' + i]
-                if (cell !== undefined) {
-                  const pages = Number(cell.v)
-                  sum += pages
-                  let date = sheet['C' + i].v.toString()
-                  date += sheet['D' + i].v.toString()
-                  date += sheet['E' + i].v.toString()
-                  const doc = {
-                    title: sheet['B' + i].v,
-                    date,
-                    pages 
-                  }
-                  docs.push(doc)
-                  count += 1
-                }
-              }
-
-              // console.log(count)
-              // console.log(sum)
-              // console.log(docs)
-
-              let arr0 = []
-              arr0[1] = name
-              arr0[6] = count
-              arr0[7] = sum
-              ws_data0.push(arr0)
-
-              let ws_data1 = [
-                [ "序号", "案卷号", "案卷级档号", "档号", "类号", "类别代号", "类别件号", '材料名称', '形成时间', '页数', '' ]
-              ]
-              let arr1 = []
-              arr1[0] = name
-              ws_data1.push(arr1)
-
-              for (let i = 1; i <= docs.length; i++) {
-                const doc = docs[i-1]
-                arr1 = []
-                arr1[0] = i
-                arr1[7] = doc.title
-                arr1[8] = doc.date
-                arr1[9] = doc.pages
-                ws_data1.push(arr1)
-              }
-
-              const wb = utils.book_new()
-              utils.book_append_sheet(wb, utils.aoa_to_sheet(ws_data1), sheetName)
-              const data = write(wb, { type: "buffer", bookType: "xlsx" })
-              await writeBinaryFile(newDir + '/' + name + '-人事卷内目录.xlsx', data)
-
-            } catch(err) {
-              console.log(err)
-            }
-          } else {
-            // if entry is dir
+            const individual = await extractData(await getSheet(entry.path))
+            console.log(individual)
+            // append AnJuan ws_data
+            // aoa = [[1,2,3,4,5],[2,3,4,5,6]]
+            
+            // export JuanNei
+            exportJuanNei(individual)
+            let file = entry
           }
         }
 
-        const wb = utils.book_new()
-        utils.book_append_sheet(wb, utils.aoa_to_sheet(ws_data0), sheetName)
-        const data = write(wb, { type: "buffer", bookType: "xlsx" })
-        await writeBinaryFile(newDir + '/人事案卷.xlsx', data)
+        // export AnJuan
+        exportAnJuan(aoa)
 
         setMsg('完成')
 
-        // open newDir
       } catch(err) {
         console.log(err)
         setMsg('只能选择目录')
@@ -212,6 +245,13 @@ function App() {
             required
             placeholder="点击选择目录或将目录拖拽到这里"
             value={dir}
+          />
+          <label>案卷级档号前缀<span className="asteroid">*</span><br/>格式：YCSY01-RST01-1</label>
+          <input
+            name="prefix"
+            className="input"
+            required
+            placeholder="填写案卷级档号前缀"
           />
           <label>人员基本信息表</label>
           <input
